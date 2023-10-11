014ef36d836dd5b641a7029465eec3adc886e8fd
arpitasen20 <arpitasen200311@gmail.com>
2023-10-11 15:47:34 +0530

    heap sort

---
+++ C/HeapSort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+// Function to heapify a subtree rooted with the given node
+void heapify(int arr[], int n, int i) {
+    int largest = i; // Initialize largest as root
+    int left = 2 * i + 1; // Left child
+    int right = 2 * i + 2; // Right child
+
+    // If left child is larger than the root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // If right child is larger than the largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If the largest is not the root, swap the root with the largest
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// The main function to sort an array of given size
+void heapSort(int arr[], int n) {
+    // Build a max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract elements from the heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move the current root to the end
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        // Call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    heapSort(arr, n);
+
+    printf("\nSorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    
+    return 0;
+}
