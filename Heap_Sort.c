e8d1b2f46bd360620a85e40e06e12f5d7284dd85
SYED ASGAR AHMED <73679838+syedasgarahmed@users.noreply.github.com>
2023-10-11 21:08:32 +0530

    Create Heap_Sort.c
    The heapify function is used to build a max heap from an array. It ensures that the largest element is at the root of the heap.
    The heapSort function first builds a max heap from the input data. Then, it repeatedly extracts the maximum element from the heap and places it at the end of the array.
    In the main function, the program reads the number of elements and the elements themselves from the user, sorts the array using heapSort, and then prints the sorted array.

---
+++ Heap_Sort.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+// Function to heapify a subtree rooted with node i which is an index in arr[].
+void heapify(int arr[], int n, int i) {
+    int largest = i; // Initialize the largest as the root
+    int left = 2 * i + 1; // Left child
+    int right = 2 * i + 2; // Right child
+
+    // If the left child is larger than the root
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    // If the right child is larger than the largest so far
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    // If the largest is not the root
+    if (largest != i) {
+        // Swap the largest with the root
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// Function to perform heap sort
+void heapSort(int arr[], int n) {
+    // Build a max heap from the input data
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // Extract elements from the heap one by one
+    for (int i = n - 1; i >= 0; i--) {
+        // Move the current root to the end
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        // Call heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    heapSort(arr, n);
+
+    printf("\nSorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
